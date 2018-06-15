7e22ff07ae33636092a67f8b6b13088df5369570
Manuel Spuhler <manuel.spuhler@chuv.ch>
2018-06-15 14:55:06 +0200

    User cors for dev

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors'
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import bodyParser from 'body-parser';
 import schema from './data/schema';
@@ -6,6 +7,7 @@ import schema from './data/schema';
 const GRAPHQL_PORT = 3000;
 
 const graphQLServer = express();
+graphQLServer.use(cors()) 
 
 graphQLServer.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
 graphQLServer.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
